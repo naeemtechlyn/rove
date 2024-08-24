3cc53ac3cebd68480c55177bc7df8fdd42cefa50
naeemtechlyn <naeem.techlyn@gmail.com>
2024-08-24 09:53:27 +0600

    project upload on github

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+(function ($) {
+    
+    "use strict"
+    $(document).ready(function () {
+
+        // mobile menu
+        $('#mobile-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "991.98",
+            onePage: false
+        });
+
+        // counter up
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1500
+        });
+
+        // magnific popup init
+        $(".popup-gallery").magnificPopup({
+            delegate: '.popup-img',
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+        });
+        $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+            type: "iframe",
+            mainClass: "mfp-fade",
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+
+        // service single
+        $('.service-single').hover(function() {
+            $('.service-single').removeClass('active');  
+            $(this).addClass('active');  
+        });        
+        $('.service-single:nth-child(2)').addClass('active');
+
+        // testimonial slider 
+        var swiper = new Swiper(".testimonial-slider", {
+            pagination: {
+                el: ".swiper-pagination",
+                type: "fraction",
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+
+    });
+
+    $(window).on('scroll', function () {
+
+        // navbar fixed
+        const headerHeight = $(".header-area").outerHeight();
+        if ($(this).scrollTop() > headerHeight) {
+            $('.menu-area').addClass("fixed-top");
+        } else {
+            $('.menu-area').removeClass("fixed-top");
+        }
+
+    });
+
+    $(window).on('load', function () {
+
+        // preloader
+        var preLoder = $("#preloader");
+        preLoder.fadeOut(0);
+
+    });
+
+})(jQuery);
